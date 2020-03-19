880876b66bb7e27fa2ef736e754f70a72dcae5f8
vxvy <erioyakigmail.com>
2020-03-19 11:00:32 +0100

    first commit

---
+++ app/src/main/java/com/example/dardos/EscenaEnPantalla.java
@@ -0,0 +1,24 @@
+package com.example.dardos;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class EscenaEnPantalla extends SurfaceView implements SurfaceView.Callback{
+    public EscenaEnPantalla(Context context) {
+        super(context);
+    }
+
+    public EscenaEnPantalla(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EscenaEnPantalla(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public EscenaEnPantalla(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+}
---
+++ app/src/main/java/com/example/dardos/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.dardos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static Context context;
+    public static boolean musica;
+    public static float duracionPartida;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try{
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }catch(Exception ignore){}
+
+        setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        int opciones = View.SYSTEM_UI_FLAG_FULLSCREEN        // pone la pantalla en modo pantalla completa ocultando elementos no criticos como la barra de estado.
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  // oculta la barra de navegación
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(opciones);
+
+        this.context = this;
+
+        //Datos partida
+        this.duracionPartida = 0f;
+
+        //Opciones
+        this.musica = true;
+
+        EscenaEnPantalla contenedorSurfaceView = new EscenaEnPantalla(this);
+        contenedorSurfaceView.setKeepScreenOn(true);
+        this.setContentView(contenedorSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Se quita el getWindow porque no hace falta en el onResume
+        try{
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }catch(Exception ignore){}
+
+        View decorView = getWindow().getDecorView();
+        int opciones = View.SYSTEM_UI_FLAG_FULLSCREEN       // pone la pantalla en modo pantalla completa ocultando elementos no criticos como la barra de estado.
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION       // oculta la barra de navegación
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(opciones);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
+
+
+}
